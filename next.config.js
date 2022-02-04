b7b960234370388f9d3121c7d9cc8641f1f6e62e
rpxs <me@rpxs.me>
2022-02-04 00:43:50 -0700

    (fix) next.config.js hotfix

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 });
 module.exports = withMDX({
-  experimental: {
-    concurrentFeatures: true,
-  },
   reactStrictMode: true,
   pageExtensions: ["js", "jsx", "mdx"],
 });
