f44cf86b4d579d7cf2ccc299d4793f72ab5a6767
rpxs <me@rpxs.me>
2022-02-04 01:07:10 -0700

    (feat) add seo

--- components/Wrapper.js
+++ components/Wrapper.js
@@ -1,6 +1,11 @@
+import Head from "next/head";
 export default function Wrapper(props) {
   return (
     <div className="bg-slate-900 text-gray-300 flex flex-col min-h-screen items-center">
+      <Head>
+        <title>{props.meta.title}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <div className="my-20 sm:text-4xl text-3xl font-bold">
         <h1>{props.meta.title}</h1>
       </div>
