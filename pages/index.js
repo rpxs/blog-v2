a0e6adabf8053be04dac906145561f1b4fd48f41
rpxs <me@rpxs.me>
2022-02-04 19:01:34 -0700

    (fix) seo

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-head-element */
 import posts from "../lib/posts";
 export const config = {
   unstable_runtimeJS: false,
@@ -5,23 +6,33 @@ export const config = {
 
 export default function Home() {
   return (
-    <div className="flex flex-col bg-slate-900 font-mono text-white min-h-screen items-center">
-      <p className="my-10 text-lg">fish&apos;s blog</p>
-      <p className="mb-10 text-white">all posts: </p>
-      <ul className="list-disc">
-        {posts.map((post, index) => (
-          <li key={index}>
-            <a
-              href={"/p/" + post.slug}
-              rel="preload"
-              className="underline sm:text-base text-sm decoration-2 text-blue-500 hover:text-blue-600"
-            >
-              {post.title} • {post.publishedAt}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <head>
+        <title>fish&apos;s blog</title>
+        <meta
+          name="description"
+          content="a blog containing the thoughts of fish."
+        />
+      </head>
+      <div className="flex flex-col bg-slate-900 font-mono text-white min-h-screen items-center">
+        <p className="my-10 text-lg">fish&apos;s blog</p>
+        <ul className="list-disc">
+          {" "}
+          all posts:
+          {posts.map((post, index) => (
+            <li key={index}>
+              <a
+                href={"/p/" + post.slug}
+                rel="preload"
+                className="underline sm:text-base text-sm decoration-2 text-blue-500 hover:text-blue-600"
+              >
+                {post.title} • {post.publishedAt}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
