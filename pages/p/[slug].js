50756478bf3e2370bbe66ca5205aee4fdc454b0d
rpxs <me@rpxs.me>
2022-03-20 17:53:36 -0600

    (feat) redis and OSS transition

---
+++ pages/p/[slug].js
@@ -0,0 +1,21 @@
+import Wrapper from "../../components/Wrapper";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+import redis from "ioredis";
+const client = new redis(process.env.REDIS_URL);
+export const config = {
+  unstable_runtimeJS: false,
+};
+export default function Post({ source }) {
+  return (
+    <Wrapper meta={source.frontmatter}>
+      <MDXRemote {...source} />
+    </Wrapper>
+  );
+}
+export async function getServerSideProps(ctx) {
+  const slug = ctx.query.slug;
+  const source = await client.hget("posts", slug);
+  const mdxSource = await serialize(source, { parseFrontmatter: true });
+  return { props: { source: mdxSource } };
+}
